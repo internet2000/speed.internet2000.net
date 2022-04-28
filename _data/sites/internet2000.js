bbaab3db12147f45f2c6407e0172cdf63e35d53d
Alex Hoyau <alex@lexoyo.me>
2022-04-28 19:25:36 +0200

    fix build

--- _data/sites/internet2000.js
+++ _data/sites/internet2000.js
@@ -1,9 +1,8 @@
 module.exports = {
-	description: "Internet 2000 websites", // optional, falls back to object key
+	description: "Internet 2000 websites",
 	description2: "Static web sites designed by [Internet 2000](https://internet2000.net), powered by [Silex](https://www.silex.me) and [11ty](https://11ty.dev)",
 	options: {
 		frequency: 60 * 23, // 24 hours
-		freshChrome: "run",
     runs: 1,
 	},
 	urls: [
@@ -14,7 +13,6 @@ module.exports = {
     "https://blog.refty.co",
     "https://belighted.com",
     "https://tournova.fr",
-    "https://internet2000.net/molydesign/",
     "https://ocr.internet2000.net/",
 	]
 };
