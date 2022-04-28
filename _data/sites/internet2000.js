9b550af8596c491763480f418e55444af6045c75
Alex Hoyau <alex@lexoyo.me>
2022-04-28 18:36:53 +0200

    our sites

---
+++ _data/sites/internet2000.js
@@ -0,0 +1,19 @@
+module.exports = {
+	description: "Internet 2000 websites", // optional, falls back to object key
+	description2: "Static web sites designed by [Internet 2000](https://internet2000.net), powered by [Silex](https://www.silex.me) and [11ty](https://11ty.dev)",
+	options: {
+		frequency: 60 * 23, // 24 hours
+		freshChrome: "run"
+	},
+	urls: [
+    "https://internet2000.net",
+    "https://cmettavant.fr",
+    "https://lescousettesdezoe.fr",
+    "https://silex.me",
+    "https://blog.refty.co",
+    "https://belighted.com",
+    "https://tournova.fr",
+    "https://internet2000.net/molydesign/",
+    "https://ocr.internet2000.net/",
+	]
+};
