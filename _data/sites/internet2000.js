f2e07478154f3b18154db1f15f2f07a8b6f2fdb8
Alex Hoyau <a.hoyau@silexlabs.org>
2022-04-29 17:10:50 +0200

    Update internet2000.js

--- _data/sites/internet2000.js
+++ _data/sites/internet2000.js
@@ -3,7 +3,7 @@ module.exports = {
 	description2: "Static web sites designed by [Internet 2000](https://internet2000.net), powered by [Silex](https://www.silex.me) and [11ty](https://11ty.dev)",
 	options: {
 		frequency: 60 * 23, // 24 hours
-    runs: 1,
+    runs: 2,
 	},
 	urls: [
     "https://internet2000.net",
@@ -13,6 +13,6 @@ module.exports = {
     "https://blog.refty.co",
     "https://belighted.com",
     "https://tournova.fr",
-    "https://ocr.internet2000.net/",
+    "https://ocr.internet2000.net/formations-financees-demandeurs-demploi/",
 	]
 };
