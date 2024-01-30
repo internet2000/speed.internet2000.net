f4d63db0f05c3beb9ca30d7fd0d7ea819931c5c9
Alex Hoyau <a.hoyau@silexlabs.org>
2024-01-30 12:56:11 +0400

    Update internet2000.js

--- _data/sites/internet2000.js
+++ _data/sites/internet2000.js
@@ -12,7 +12,7 @@ module.exports = {
 		"https://www.silex.me",
 		"https://tournova.fr",
 		"moly.internet2000.net",
-		"https://ocr.internet2000.net/formations-financees-demandeurs-demploi/", "https://info.openclassrooms.com/fr-fr/fr/lp/formations-financees-demandeurs-demploi",
+		"https://ocr.internet2000.net/formations-financees-demandeurs-demploi/",
 		"https://hakilidesign.com/",
 		"https://eqwalgroup.com/",
                 "https://kalikadoproduits.fr/",
