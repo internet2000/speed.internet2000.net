038061187d1ad980a1f55906ac3992134a79a61e
Alex Hoyau <a.hoyau@silexlabs.org>
2024-01-30 12:55:49 +0400

    Update internet2000.js

--- _data/sites/internet2000.js
+++ _data/sites/internet2000.js
@@ -6,17 +6,20 @@ module.exports = {
     runs: 3,
 	},
 	urls: [
-		"https://belighted.com",
 		"https://internet2000.net",
 		"https://cmettavant.fr",
 		"https://lescousettesdezoe.fr",
 		"https://www.silex.me",
 		"https://tournova.fr",
+		"moly.internet2000.net",
 		"https://ocr.internet2000.net/formations-financees-demandeurs-demploi/", "https://info.openclassrooms.com/fr-fr/fr/lp/formations-financees-demandeurs-demploi",
 		"https://hakilidesign.com/",
 		"https://eqwalgroup.com/",
-		"https://softcorner.internet2000.net/", "https://www.softcorner.eu/",
+                "https://kalikadoproduits.fr/",
+		"https://act.surfrider.eu/",
 		"https://scoreddb.fr/",
-                "https://kalikadoproduits.fr/"
+		"https://clay-group.fr/",
+		"https://www.nectar-io.fr/",
+		"https://eco-starter.eco2.i2k.site/"
 	]
 };
