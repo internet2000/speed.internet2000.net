206100a6ec3537e1e3111eaaeee2d0c89815e7e6
Alex Hoyau <a.hoyau@silexlabs.org>
2024-09-27 11:32:45 +0300

    split per year

---
+++ _data/sites/2023.js
@@ -0,0 +1,18 @@
+module.exports = {
+	description: "Internet 2000 websites of 2023",
+	description2: "Static web sites designed by [Internet 2000](https://internet2000.net), powered by [Silex](https://www.silex.me) and [11ty](https://11ty.dev)",
+	options: {
+		frequency: 60 * 23, // 24 hours
+		runs: 3,
+	},
+	urls: [
+		"https://internet2000.net",
+		"https://cmettavant.fr",
+		"https://tournova.fr",
+		"https://moly.internet2000.net",
+		"https://ocr.internet2000.net/formations-financees-demandeurs-demploi/",
+		"https://hakilidesign.com/",
+		"https://eqwalgroup.com/",
+		"https://kalikadoproduits.fr/",
+	]
+};
