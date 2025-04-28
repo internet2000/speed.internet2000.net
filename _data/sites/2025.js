c1c9cdeb1303cee018b2f9b13d7ca0c5b0dbcdb3
MOREAU Jonas <102809203+MoreauJonas@users.noreply.github.com>
2025-04-28 15:18:07 +0200

    Create 2025.js
    add 2025 and site

---
+++ _data/sites/2025.js
@@ -0,0 +1,17 @@
+module.exports = {
+	description: "Internet 2000 websites of 2025",
+	description2: "Static web sites designed by [Internet 2000](https://internet2000.net), powered by [Silex](https://www.silex.me) and [11ty](https://11ty.dev)",
+	options: {
+		frequency: 60 * 23, // 24 hours
+		runs: 3,
+	},
+	urls: [
+		"https://fresquedelarestauration.fr/",
+		"https://www.leasecom.fr/",
+		"https://hazelle-conseil.fr/",
+		"https://www.tendron.com/",
+		"https://sfgg-events.org/",
+		"https://www.tendron.com/",
+		"https://icidcongress2026.org/",
+	]
+};
